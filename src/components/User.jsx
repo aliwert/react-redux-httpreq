07c34f9430ccf3c592edf0022c6ad80c77f6a001
aliwert <154356044+aliwert@users.noreply.github.com>
2024-05-08 19:24:17 +0300

    displayflex and gap added

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,11 +1,11 @@
 const User = ({ user }) => {
   return (
     <>
-      <div>
-        <div>{user.id}</div>
-        <div>{user.name}</div>
-        <div>{user.username}</div>
-        <div>{user.email}</div><br /><br />
+      <div style={{display: 'flex', gap: 20}}>
+        <div>Id:{user.id}</div>
+        <div>Name:{user.name}</div>
+        <div>Username:{user.username}</div>
+        <div>Email:{user.email}</div><br /><br />
         <div></div>
       </div>
     </>
